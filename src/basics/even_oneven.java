d4659ead5bb9799a01e7a2757a7674cf47ee4672
Charlotte-Schroer <charlotte.schroer@student.ehb.be>
2024-03-28 09:56:11 +0100

    Programming Essentials 2

--- src/basics/even_oneven.java
+++ src/basics/even_oneven.java
@@ -1,4 +1,17 @@
 package basics;
-
+//Schrijf code waarmee je alle even getallen tussen 36 en 60 kan afdrukken. Maak hiervoor gebruik van een for loop.
 public class even_oneven {
+
+    public static void main(String[] args){
+        int getal1 = 36;
+        int getal2 = 60;
+
+        for(int i = getal1; i<=getal2; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+
+
 }
