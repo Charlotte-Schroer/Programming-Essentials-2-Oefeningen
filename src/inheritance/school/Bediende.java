92ac2078df4d8267faf9ca838526a1c053bcc663
Charlotte-Schroer <charlotte.schroer@student.ehb.be>
2024-04-06 16:39:14 +0200

    functie lokaal wijzigigen door bediende toegevoegd

--- src/inheritance/school/Bediende.java
+++ src/inheritance/school/Bediende.java
@@ -4,6 +4,9 @@ public class Bediende extends Personeel{
     public Bediende(String voornaam, String naam, Lokaal lokaal){
         super(voornaam, naam, lokaal);
     }
+    protected void setLokaal(Lokaal lokaal, Personeel personeelslid){
+        personeelslid.setLokaal(lokaal);
+    }
     @Override
     public String toString(){
         return "Bediende" + getVoornaam()+" " + getAchternaam() +"\nLokaal: " + getLokaal();
